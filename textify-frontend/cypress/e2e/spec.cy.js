9d20f0db0a189cf56d2b503cccaa4e75c5876ec0
Stepan Mikhailiuk <istepancar@gmail.com>
2024-10-18 12:33:17 +0800

    feat(*): only e2e

--- textify-frontend/cypress/e2e/spec.cy.js
+++ textify-frontend/cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
+    cy.visit('https://ex.cypress.io')
+  })
+  it.only('passes', () => {
     cy.visit('https://example.cypress.io')
   })
 })
