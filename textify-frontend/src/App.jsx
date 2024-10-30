6feded43f4886451f7ad2caed9d3093f6f0050f3
Valeriia <valyavina.lera@yandex.ru>
2024-10-30 14:13:36 +0300

    Update App.jsx

--- textify-frontend/src/App.jsx
+++ textify-frontend/src/App.jsx
@@ -1,12 +1,8 @@
-import ProjectCard from "./shared/ui/project-card/ProjectCard";
-import PageSettings from "./widgets/header/settings/settings/PageSettings";
-
-
 const App = () =>  {
-    return <>
-    <PageSettings/>
-        {/* <ProjectCard imageUrl={'https://ir-3.ozone.ru/s3/multimedia-m/c1000/6726566866.jpg'} lastEntryTime={new Date(2024, 9)} isRemoved={false}  /> */}
-    </>;
-}
+    return (
+        <>
+        </>
+    );
+};
 
 export default App;
