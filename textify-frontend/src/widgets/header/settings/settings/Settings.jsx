b0b3cfe1a388b888d121f508c86ac4589be4f863
Valeriia <valyavina.lera@yandex.ru>
2024-10-30 23:46:09 +0300

    merge two setting

--- textify-frontend/src/widgets/header/settings/settings/Settings.jsx
+++ textify-frontend/src/widgets/header/settings/settings/Settings.jsx
@@ -1,11 +1,14 @@
 import { useState, useCallback } from 'react';
-import {Box,Button, Menu, MenuList} from '@mui/material';
+import {Box, Button, Menu, MenuList} from '@mui/material';
 import './Settings.css';
+import Search from "../../../../shared/ui/search-bar/Search.jsx";
 import ButtDel from "../butt/ButtDel.jsx";
 import SwitchButton from "../switch-button/SwitchButton.jsx";
 import ButtonInOut from "../butt/ButtonInOut.jsx";
+import PropTypes from 'prop-types';
 
-const TrashSettings = () => {
+
+const Settings = ({isTrash}) => {
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
 
@@ -40,19 +43,26 @@ const TrashSettings = () => {
             >
                 <MenuList className="settings-case_name">Настройки</MenuList>
 
-                <ButtDel id="settings-case_custom-button" placeholder="Цвет фона"/>
+                { !isTrash &&(
+                    <Search className="settings-case_search" placeholder="Поиск по файлу"/>
+                )}
+                { !isTrash &&(
+                    <ButtDel id="settings-case_custom-button" placeholder="Удалить проект"/>
+                
+                )}
+                    <ButtDel id="settings-case_custom-button" placeholder="Цвет фона"/>
 
-                <ButtDel id="settings-case_custom-button" placeholder="Размер шрифта"/>
+                    <ButtDel id="settings-case_custom-button" placeholder="Размер шрифта"/>
 
-                <ButtDel id="settings-case_custom-button" placeholder="Набор шрифтов"/>
+                    <ButtDel id="settings-case_custom-button" placeholder="Набор шрифтов"/>
                 <MenuList id="settings-case_theme">
                     <p>Тема:</p>
                     <SwitchButton className="settings-case-theme-switch" />
                 </MenuList>
-                <ButtonInOut className="settings-case_button-in-out" placeholder="Выход" />
+                    <ButtonInOut className="settings-case_button-in-out" placeholder="Выход" />
             </Menu>
         </Box>
     );
 };
 
-export default TrashSettings;
+export default Settings;
