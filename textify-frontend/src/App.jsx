bc323ae01a4bb129d3c09afd6bb06e251b99a1dc
Valiavina Valeriia <115079868+ValeriyaKa@users.noreply.github.com>
2024-10-30 13:49:49 +0300

    solve conflict

--- textify-frontend/src/App.jsx
+++ textify-frontend/src/App.jsx
@@ -4,8 +4,8 @@ import Search from "./shared/ui/search-bar/Search";
 
 const App = () =>  {
     return <>
-    <Search/>
-        {/* <ProjectCard imageUrl={'https://ir-3.ozone.ru/s3/multimedia-m/c1000/6726566866.jpg'} lastEntryTime={new Date(2024, 9)} isRemoved={false}  /> */}
+    
+        { <ProjectCard imageUrl={'https://ir-3.ozone.ru/s3/multimedia-m/c1000/6726566866.jpg'} lastEntryTime={new Date(2024, 9)} isRemoved={false}  > */}
     </>;
 }
 
