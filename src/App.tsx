752b7c9ae1593d5f343f6c068717a66763d44236
“bassceo” <meregodz@gmail.com>
2024-11-07 13:50:02 +0300

    feature: First Demo Editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,10 @@
-import RouterBreadcrumbs from "./shared/ui/pages-tree/PagesTree";
+import Editor from "./widgets/editor/Editor";
 
-const tree = [
-    {
-      name: 'Главная',
-      type: 'link',
-      link: '/main'
-    },
-    {
-      name: 'Создать проект',
-      type: 'action',
-      action: 'create',
-      icon: 'plus'
-    },
-    {
-      name: 'Nomer 1',
-      type: 'dropdown',
-      link: '/nomer1',
-      items: [
-        { name: 'Page 1', type: 'link', link: '/nomer1/page1' },
-        { name: 'Page 2', type: 'link', link: '/nomer1/page2' },
-        { name: 'Page 3', type: 'link', link: '/nomer1/page3' }
-      ]
-    },
-    {
-      name: 'Nomer 2',
-      type: 'dropdown',
-      link: '/nomer2',
-      items: [
-        { name: 'Page 1', type: 'link', link: '/nomer2/page1' },
-        { name: 'Page 2', type: 'link', link: '/nomer2/page2' },
-        { name: 'Page 3', type: 'link', link: '/nomer2/page3' }
-      ]
-    }
-  ];
-  
-  const App = () => {
-    return (
-      <div>
-        <RouterBreadcrumbs tree={tree} />
-      </div>
-    );
-  };
+const App = () => {
+
+  return (
+    <Editor />
+  );
+};
 
 export default App;
