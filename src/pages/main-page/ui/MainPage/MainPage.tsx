59d80b1e755e108cc0983ff53c4ce99d50cb92c6
ilya <116213965+kag3yma@users.noreply.github.com>
2024-11-26 01:44:57 +0300

    fix

--- src/pages/main-page/ui/MainPage/MainPage.tsx
+++ src/pages/main-page/ui/MainPage/MainPage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './MainPage.scss';
 import LayoutWrapper from '../../../layout-wrapper/ui/LayoutWrapper/LayoutWrapper';
+import LastProjectCard from '../../../../shared/ui/last-project-card/LastProjectCard';
+
 const MainPage: React.FC = () => {
     
 
@@ -12,12 +14,18 @@ const MainPage: React.FC = () => {
                 </p>
             </div>
             <div>
-
+                <LastProjectCard
+                    title="Nomer 1"
+                    imageUrl="https://via.placeholder.com/150"
+                />
             </div>
             <div>
-            <p className="your-projects text">
-            Ваши проекты
-            </p>
+                <p className="your-projects text">
+                Ваши проекты
+                </p>
+            </div>
+            <div>
+            
             </div>
         </LayoutWrapper>
     );
---
+++ src/shared/ui/last-project-card/LastProjectCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card, CardActionArea, CardContent, CardMedia } from '@mui/material';
+import './LastProjectCard.scss';
+
+interface LastProjectCardProps {
+    title: string;
+    imageUrl: string;
+}
+
+const LastProjectCard: React.FC<LastProjectCardProps> = ({ 
+    title, 
+    imageUrl 
+}) => {
+    return (
+        <Card className="last-project-card">
+            <CardActionArea disableRipple className="last-project-card__action">
+                <CardMedia className='last-project-card__image'
+                    component="img"
+                    image={imageUrl}
+                    alt={title}
+                />
+                <CardContent sx={{padding: '3px 0px 0px 0px',}} className="last-project-card__content">
+                <div>{title}</div>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+};
+
+export default LastProjectCard;
