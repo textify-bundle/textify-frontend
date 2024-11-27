836c9f61040037ee3a6c4371698dd39ba49f99b4
ilya <116213965+kag3yma@users.noreply.github.com>
2024-11-27 16:26:23 +0300

    another fix aaaaaaaa

--- src/pages/layout-wrapper/ui/LayoutWrapper/ts/interfaces.ts
+++ src/pages/layout-wrapper/ui/LayoutWrapper/ts/interfaces.ts
@@ -1,7 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-type LayoutType = 'main' | 'project';
-
 export interface ILayoutWrapperProps {
-    layout: LayoutType[];
+    layout: string;
     children?: React.ReactNode;
 }
