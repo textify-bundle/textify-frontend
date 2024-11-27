ea136d1230428e8e6176b3676d1fbacbd7fb71e0
ilya <116213965+kag3yma@users.noreply.github.com>
2024-11-27 16:19:48 +0300

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,7 +7,6 @@ import { AppDispatch } from "../store";
 import { restoreSession, refreshTokens } from "../store/slices/authSlice";
 import { supabase } from "../utils/client";
 import './styles/index.scss';
-import { MainPage } from "../pages/main-page";
 import LayoutWrapper from "../pages/layout-wrapper/ui/LayoutWrapper/LayoutWrapper";
 
 const App: React.FC = () => {
@@ -31,7 +30,7 @@ const App: React.FC = () => {
     <Routes>
             <Route path="/" element={<AuthPage />} />
             <Route element={<ProtectedRoute />}>
-                <Route path="/main" element={<LayoutWrapper layout={['main']}><MainPage /></LayoutWrapper>} />
+                <Route path="/main" element={<LayoutWrapper layout={'main'}></LayoutWrapper>} />
             </Route>
         </Routes>
   );
--- src/pages/layout-wrapper/ui/LayoutWrapper/ts/interfaces.ts
+++ src/pages/layout-wrapper/ui/LayoutWrapper/ts/interfaces.ts
@@ -1,7 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-type LayoutType = 'main' | 'project' | 'trash';
 
 export interface ILayoutWrapperProps {
-    layout: LayoutType[];
+    layout: string;
     children?: React.ReactNode;
 }
