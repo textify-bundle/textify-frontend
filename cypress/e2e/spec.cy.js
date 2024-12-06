1c730ca35ba6a27f502641a74239ae95eb40e6e4
ilya <116213965+kag3yma@users.noreply.github.com>
2024-12-06 17:57:27 +0300

    e2e

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    cy.intercept('GET', 'https://example.com/api/data', { fixture: 'mockData.json' }).as('getData');
+    cy.visit('https://example.cypress.io');
+    cy.wait('@getData').then((interception) => {
+      expect(interception.response.body.data.name).to.eq('Mock Item');
+    });
+  });
+});
