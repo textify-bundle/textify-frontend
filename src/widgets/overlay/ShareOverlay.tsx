8520bab26555985ab4ec303824de4639c76cedc7
ValeriyaKa <Valyavinalera.090101@mail.ru>
2025-02-05 20:07:06 +0300

    redone and corrected styles

--- src/widgets/overlay/ShareOverlay.tsx
+++ src/widgets/overlay/ShareOverlay.tsx
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Typography, Button, Dialog, DialogActions, DialogContent, DialogTitle, Accordion, AccordionSummary, AccordionDetails, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel } from '@mui/material';
+import {
+  Box,
+  Typography,
+  Button,
+  Dialog,
+  DialogContent,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  IconButton,
+} from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import CloseIcon from '@mui/icons-material/Close';
 import './ShareOverlay.scss';
 
 interface PageShareProps {
@@ -12,7 +27,7 @@ const ShareOverlay: React.FC<PageShareProps> = ({ title = "Отправить", 
   const [selectedItem, setSelectedItem] = useState<string>('Нет доступа');
   const [expanded, setExpanded] = useState<string | false>(false);
   const [generatedLink, setGeneratedLink] = useState<string>('');
-  const [openDialog, setOpenDialog] = useState<boolean>(false);  
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
 
   useEffect(() => {
     setGeneratedLink(link || `${window.location.origin}/shared`);
@@ -28,109 +43,70 @@ const ShareOverlay: React.FC<PageShareProps> = ({ title = "Отправить", 
 
   const handleCopyLink = () => {
     navigator.clipboard.writeText(generatedLink)
-      .then(() => {
-        console.log("Link copied to clipboard!");
-        console.log(generatedLink);
-      })
-      .catch(err => {
-        console.error("Failed to copy link", err);
-      });
-  };
-
-  const handleOpenDialog = () => {
-    setOpenDialog(true);   
-  };
-
-  const handleCloseDialog = () => {
-    setOpenDialog(false); 
+      .then(() => console.log("Link copied to clipboard!"))
+      .catch(err => console.error("Failed to copy link", err));
   };
 
   return (
-    <div>
-       <Button
-        variant="text"   
-        onClick={handleOpenDialog}
-        sx={{
-          position:'absolute',
-          right:267,
-          top:12,
-          textTransform: 'none',  
-          fontWeight: 'normal',   
-          padding: 0,           
-          color: 'black',       
-          minWidth: 'auto',      
-          '&:hover': {
-            backgroundColor: 'transparent', 
-          },
-          marginLeft: '10px',   
-        }}
-      >
+    <div className='container'>
+      <Button className="send-button" onClick={() => setOpenDialog(true)}>
         {title}
       </Button>
 
-       <Dialog open={openDialog} onClose={handleCloseDialog} fullWidth maxWidth="sm">
-        <DialogTitle>{title}</DialogTitle>
-        <DialogContent>
-          <Box className={`overlay-to-share${expanded ? ' expanded' : ''}`}>
-            <Typography variant="body1" className="overlay-to-share__access-label" sx={{ marginRight: "110px" }}>
-              У кого есть ссылка:
+      <Dialog open={openDialog} onClose={() => setOpenDialog(false)} fullWidth maxWidth="xs" className="share-dialog">
+        <DialogContent className="share-dialog__content">
+          <Box className="share-dialog__header">
+            <Typography variant="h6">Поделиться</Typography>
+            <IconButton onClick={() => setOpenDialog(false)} className="share-dialog__close-btn">
+            <CloseIcon sx={{ color: '#d5d4d4', '&:hover': { color: '#dfdbdb' } }} />
+            </IconButton>
+          </Box>
+          <Box className="share-dialog__access">
+            <Typography variant="body1" className="share-dialog__label">
+              У кого есть ссылка
             </Typography>
             <Accordion
-              data-testid="accordion-header"
               expanded={expanded === "panel1"}
               onChange={handleChange("panel1")}
-              className="overlay-to-share__accordion"
+              className="share-dialog__accordion"
             >
               <AccordionSummary
-                className='overlay-to-share_panel1-header'
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1-content"
-                id="panel1-header"
+                className="share-dialog__accordion-summary"
               >
                 {selectedItem}
               </AccordionSummary>
-              <AccordionDetails>
+              <AccordionDetails className="share-dialog__accordion-details">
                 <FormControl>
-                  <FormLabel id="access-radio-buttons-group-label">Доступ</FormLabel>
-                  <RadioGroup
-                    aria-labelledby="access-radio-buttons-group-label"
-                    value={selectedItem}
-                    onChange={handleItemChange}
-                    name="access-radio-buttons-group"
-                  >
-                    <FormControlLabel
-                      value="Нет доступа"
-                      control={<Radio />}
-                      label="Нет доступа"
-                    />
-                    <FormControlLabel
-                      value="Только чтение"
-                      control={<Radio />}
-                      label="Только чтение"
-                    />
-                    <FormControlLabel
-                      value="Редактирование"
-                      control={<Radio />}
-                      label="Редактирование"
-                    />
+                  <RadioGroup value={selectedItem} onChange={handleItemChange}
+                  sx={{
+                    '& .MuiSvgIcon-root': {
+                      fontSize: 18, 
+                      borderRadius: '50%', 
+                      border: '1px solid #e0e0e0',
+                      backgroundColor: 'transparent',
+                      '&:checked': {
+                        color: '#007bff', 
+                      }
+                    }
+                  }} >
+                    <FormControlLabel value="Нет доступа" control={<Radio />} label="Нет доступа" />
+                    <FormControlLabel value="Только чтение" control={<Radio />} label="Только чтение" />
+                    <FormControlLabel value="Редактирование" control={<Radio />} label="Редактирование" />
                   </RadioGroup>
                 </FormControl>
               </AccordionDetails>
             </Accordion>
-            <Button
-              variant="contained"
-              className="overlay-to-share__button"
-              onClick={handleCopyLink}
-            >
-              Копировать ссылку
-            </Button>
           </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseDialog} color="primary">
-            Закрыть
+
+          <Button
+            variant="contained"
+            className="share-dialog__copy-btn"
+            onClick={handleCopyLink}
+          >
+            Копировать ссылку
           </Button>
-        </DialogActions>
+        </DialogContent>
       </Dialog>
     </div>
   );
