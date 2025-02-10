033e1bb09f1a6c59a9507d325f40c5d0f7dd4749
poplopok <smirnovvigev@gmail.com>
2025-02-10 15:39:18 +0300

    fds

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,42 +1,16 @@
-import React, { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import { AuthPage } from "../pages/auth/";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../store";
-import { restoreSession, refreshTokens } from "../store/slices/authSlice";
-import { supabase } from "../utils/client";
-import './styles/index.scss';
-import LayoutWrapper from "../pages/layout-wrapper/ui/LayoutWrapper/LayoutWrapper";
- 
-const App: React.FC = () => {
-  const dispatch = useDispatch<AppDispatch>();
+import React from 'react';
+import ProductList from './ProductList';
 
-  useEffect(() => {
-    dispatch(restoreSession());
+  function App(){
+    return(
+      <div>
+        <h1>
+          Туду лист
+        </h1>
+        <ProductList/>
+      </div>
 
-    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
-      if (event === "TOKEN_REFRESHED" && session) {
-        dispatch(refreshTokens());
-      }
-    });
+    )
+  }
 
-    return () => {
-      authListener.subscription.unsubscribe();
-    };
-  }, [dispatch]);
-
-  return (
-    <Routes>
-      <Route path="/" element={<AuthPage />} />
-      <Route element={<ProtectedRoute />}>
-        <Route path="/main" element={<LayoutWrapper layout={'main'}></LayoutWrapper>} />
-        <Route path="/trash" element={<LayoutWrapper layout={'trash'}></LayoutWrapper>} />
-        <Route path="/:projectId" element={<LayoutWrapper layout={'project'}></LayoutWrapper>} />
-      </Route>
-    </Routes>
-  );
-};
-
-export default App;
-
+ export default App
