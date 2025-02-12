bf597911f2eb6c4c4397828d0fa24e98b7da75af
ilya <116213965+kag3yma@users.noreply.github.com>
2025-02-12 15:37:02 +0300

    add delet with backspace and button delet

--- src/widgets/editor/node/text-editor/TextEditor.tsx
+++ src/widgets/editor/node/text-editor/TextEditor.tsx
@@ -1,7 +1,8 @@
-import React, { forwardRef, useState } from 'react';
+import React, { forwardRef, useState, useEffect } from 'react';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
 import './TextEditor.scss';
 import { NodeContent, NodeStyles } from '../../../../shared/types/editor/node';
+import { useDispatch } from 'react-redux';
 
 interface TextEditorProps {
   content: NodeContent;
@@ -9,11 +10,14 @@ interface TextEditorProps {
   onContentChange: (newContent: NodeContent) => void;
   onEnterPress: () => void;
   inputId?: string;
+  nodeId: string;
+  onDelete?: () => void;
 }
 
 const TextEditor = forwardRef<HTMLTextAreaElement, TextEditorProps>(
-  ({ content, styles, inputId, onContentChange, onEnterPress }, ref) => {
+  ({ content, styles, inputId, onContentChange, onEnterPress, nodeId, onDelete }, ref) => {
     const [value, setValue] = useState<string>(typeof content === 'string' ? content : '');
+    const dispatch = useDispatch();
 
     const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
       const newValue = event.target.value;
@@ -25,8 +29,17 @@ const TextEditor = forwardRef<HTMLTextAreaElement, TextEditorProps>(
       if (event.key === 'Enter' && !event.shiftKey) {
         event.preventDefault();
         onEnterPress();
+      } else if (event.key === 'Backspace' && value === '' && !event.shiftKey) {
+        event.preventDefault();
+        if (onDelete) {
+          onDelete();
+        }
       }
     };
+
+    useEffect(() => {
+      setValue(typeof content === 'string' ? content : '');
+    }, [content]);
 
     const renderContent = () => {
       if (typeof content === 'string') {
