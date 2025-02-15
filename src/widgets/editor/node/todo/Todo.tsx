d3b544e6834d6623b13ba43ae4d6865e7d9598a6
ilya <116213965+kag3yma@users.noreply.github.com>
2025-02-15 17:14:35 +0300

    add ToDo

--- src/widgets/editor/node/todo/Todo.tsx
+++ src/widgets/editor/node/todo/Todo.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './Todo.scss';
 
 interface TodoProps {
   content: string;
   checked: boolean;
-  onContentChange: (newContent: string) => void; // Укажите тип newContent как string
+  onContentChange: (newContent: string) => void;
   onCheckboxChange: (isChecked: boolean) => void;
 }
 
 const Todo: React.FC<TodoProps> = ({ content, checked, onContentChange, onCheckboxChange }) => {
   const [isChecked, setIsChecked] = useState(checked);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newChecked = event.target.checked;
@@ -20,7 +21,18 @@ const Todo: React.FC<TodoProps> = ({ content, checked, onContentChange, onCheckb
   const handleContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const newContent = event.target.value;
     onContentChange(newContent);
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
   };
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [content]);
 
   return (
     <div className="todo-container">
@@ -31,12 +43,16 @@ const Todo: React.FC<TodoProps> = ({ content, checked, onContentChange, onCheckb
         className="todo-checkbox"
       />
       <textarea
+        ref={textareaRef}
         value={content}
         onChange={handleContentChange}
         className={`todo-text ${isChecked ? 'checked' : ''}`}
         style={{
           textDecoration: isChecked ? 'line-through' : 'none',
           opacity: isChecked ? 0.5 : 1,
+          width: '100%',
+          resize: 'none',
+          overflow: 'hidden',
         }}
       />
     </div>
