00156961e71d660d8aaa7b9e294e9f4241c7c2bc
poplopok <smirnovvigev@gmail.com>
2025-02-16 19:20:15 +0300

    fix

--- src/widgets/editor/node/NodeContainer.tsx
+++ src/widgets/editor/node/NodeContainer.tsx
@@ -13,9 +13,11 @@ import {
   CustomNode,
   MediaContent,
   NodeType,
+  TableContent,
 } from '../../../shared/types/editor/node';
 import './NodeContainer.scss';
 import TextEditor from './text-editor/TextEditor';
+import Table from './table/table';
 import Divider from './divider/Divider';
 import Todo from './todo/Todo';
 import {
@@ -318,6 +320,11 @@ const NodeContainer: React.FC<NodeContainerProps> = ({ node, isNewNode }) => {
           />
         ) : node.type === 'image' ? (
           <ImageEditor content={mediaContent} nodeId={node.id} />
+        ) : node.type === 'table' ? (
+          <Table
+            content={node.content as TableContent}
+            onContentChange={(newContent) => handleContentChange(newContent)}
+          />
         ) : (
           <TextEditor
             inputId={`node-${node.id}`}
